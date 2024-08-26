d662f752d4218a09ad3a153724e0c46041240e69
opilane <opilane@HP-CZC2349HTC>
2024-08-26 16:13:58 +0300

    Add project files.

---
+++ TARpv23_CSharp/MainClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpv23_CSharp
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        { 
+            //I.osa Andmetüübid, Alamfunktsioonid, If
+            Console.OutputEncoding=Encoding.UTF8;
+            Console.WriteLine("Hello, World!");
+            string nimetus = "Python";
+            Console.WriteLine("Hello, {0}",nimetus);
+            funktsioonid.Tere(nimetus);
+            Console.Write("Sisesta esimene arv: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Sisesta teine arv: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Sisestage tegevusmärk: ");
+            märk = Console.
+            int vastus = funktsioonid.Liitmine(a, b);
+            Console.WriteLine(vastus);
+            double arv = 5.123456;
+            vastus=funktsioonid.Liitmine(a,(int)arv);
+            Console.WriteLine(vastus);
+            char taht = 'A';
+            if (vastus == 0)
+            {
+                Console.WriteLine(taht);
+            }
+            else
+            {
+                Console.WriteLine(vastus);
+            }
+            // Loo Arvuta() funktsioon, mis sõltub 3 parameetrist: teha, arv1, arv2, kasuta if konstruktsioon. Tulemus kuva ekraanile.
+        }
+    }
+}
---
+++ TARpv23_CSharp/funktsioonid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TARpv23_CSharp
+{
+    public class funktsioonid
+    {
+        public static void Tere(string nimi)
+        {
+            Console.WriteLine("Tere kallis {0}",nimi);
+        }
+        public static int Liitmine(int arv1, int arv2)
+        {
+            return arv1 + arv2; 
+        }
+        public static int Lahutamine(int arv1, int arv2)
+        {
+            return arv1 - arv2;
+        }
+        public static int Jagamine(int arv1, int arv2)
+        {
+            return arv1 / arv2;
+        }
+        public static int Korrutamine(int arv1, int arv2)
+        {
+            return arv1 * arv2;
+        }
+    }
+}
