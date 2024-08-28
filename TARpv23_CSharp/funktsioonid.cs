9a0e31a8277409d0cc130ace229e6c20d4951822
DariaHalchenko <154507353+DariaHalchenko@users.noreply.github.com>
2024-08-28 18:26:38 +0300

    Update funktsioonid.cs

--- TARpv23_CSharp/funktsioonid.cs
+++ TARpv23_CSharp/funktsioonid.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,23 +10,57 @@ namespace TARpv23_CSharp
     {
         public static void Tere(string nimi)
         {
-            Console.WriteLine("Tere kallis {0}",nimi);
+            Console.WriteLine("Tere kallis {0}", nimi);
         }
         public static int Liitmine(int arv1, int arv2)
         {
-            return arv1 + arv2; 
+            return arv1 + arv2;
         }
-        public static int Lahutamine(int arv1, int arv2)
+        public static string Pikkuse_analuus(double pikkus)
         {
-            return arv1 - arv2;
+            string analuus;
+            if (pikkus < 1.6)
+            {
+                analuus = "Lühika kasvu inimene";
+            }
+            else if (pikkus < 1.8)
+            {
+                analuus = "Keskmine kasvu inimene";
+            }
+            else
+            {
+                analuus = "Pikka kasvu inimene";
+            }
+            return analuus;
         }
-        public static int Jagamine(int arv1, int arv2)
+        //päeva number-> päeva nimetus 
+        public static string Paevad(int nr)
         {
-            return arv1 / arv2;
+            string paev;
+            switch (nr)
+            {
+                case 1: paev = "Esmaspäev"; break;
+                case 2: paev = "Teisipäev"; break;
+                case 3: paev = "Kolmapäev"; break;
+                case 4: paev = "Neljapäev"; break;
+                case 5: paev = "Reede"; break;
+                case 6: paev = "Laupäev"; break;
+                case 7: paev = "Pühapäev"; break;
+                default:
+                    paev = "Ei saa määrata";
+                    break;
+            }
+            return paev;
         }
-        public static int Korrutamine(int arv1, int arv2)
+        public static int[] Arvude_massiv(int N, int M)
         {
-            return arv1 * arv2;
+            int[] arvud = new int[M - N];
+            for (int i = 0; i < arvud.Length; i++) 
+            {
+                arvud[i] = N;
+                N++;
+            }
+            return arvud;
         }
     }
 }
