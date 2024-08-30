f4cad750327e76fbe74a66de84cab3c526db2aab
DariaHalchenko <154507353+DariaHalchenko@users.noreply.github.com>
2024-08-30 09:10:12 +0300

    Update MainClass.cs

--- TARpv23_CSharp/MainClass.cs
+++ TARpv23_CSharp/MainClass.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 namespace TARpv23_CSharp
@@ -39,29 +41,29 @@ namespace TARpv23_CSharp
             Console.Write("Mis su nimi on? ");
             string eesnimi = Console.ReadLine();
             Console.WriteLine("Tere, " + eesnimi);
-            if (eesnimi.ToLower() == "juku") 
+            if (eesnimi.ToLower() == "juku")
             {
                 Console.WriteLine("Tule minu juurde külla!");
-                int vanus = Funktsioonid.Mitu_aastat_vana(); 
+                int vanus = Funktsioonid.Mitu_aastat_vana();
                 Funktsioonid.Piletid(vanus);
             }
             else
             {
                 Console.WriteLine("Täna mind kodus pole!");
-            }  
+            }
 
             // 2. Küsi kahe inimese nimed ning teata, et nad on täna pinginaabrid
             Console.Write("Mis on sinu nimi? ");
             string nimi_1 = Console.ReadLine();
             Console.Write("Mis on sinu nimi? ");
             string nimi_2 = Console.ReadLine();
-            if (nimi_1 == nimi_2) 
+            if (nimi_1 == nimi_2)
             {
                 Console.WriteLine("Samad nimed");
             }
             else
             {
-                Console.WriteLine("Täna " +nimi_1+ " ja " +nimi_2+ " naabrid pingil "); 
+                Console.WriteLine("Täna " + nimi_1 + " ja " + nimi_2 + " naabrid pingil ");
             }
 
             // 3.Küsi ristkülikukujulise toa seinte pikkused ning arvuta põranda pindala. Küsi kasutajalt remondi tegemise soov, kui ta on positiivne, siis küsi kui palju maksab ruutmeeter ja leia põranda vahetamise hind
@@ -73,7 +75,7 @@ namespace TARpv23_CSharp
             int põrandaPindala = Funktsioonid.Põranda_pindala(pikkus_1, laius_1);
             Console.WriteLine("Põranda pindala: {0} ruutmeetrit", põrandaPindala);
             Console.Write("Kas soovite remonti teha? Jah, Ei => ");
-            string remont = Console.ReadLine().ToLower(); 
+            string remont = Console.ReadLine().ToLower();
             if (remont == "jah")
             {
                 Console.Write("Kui palju see ruutmeeter maksab? ");
@@ -85,12 +87,12 @@ namespace TARpv23_CSharp
             else
             {
                 Console.WriteLine("Ma ei vaja remonti.");
-            } 
+            }
 
             // 5. Küsi temperatuur ning teata, kas see on üle kaheksateistkümne kraadi (soovitav toasoojus talvel).
             Console.Write("Mis su temperatuur on? ");
             int temperatuuri = int.Parse(Console.ReadLine());
-            if (temperatuuri > 18) 
+            if (temperatuuri > 18)
             {
                 Console.WriteLine("Temperatuur üle 18 kraadi");
             }
@@ -98,7 +100,7 @@ namespace TARpv23_CSharp
             {
                 Console.WriteLine("Temperatuur mitte üle 18 kraadi");
                 Console.WriteLine("See on talvel soovitatav temperatuur");
-            } 
+            }
 
             // 6. Küsi inimese pikkus ning teata, kas ta on lühike, keskmine või pikk (piirid pane ise paika)
             try
@@ -107,7 +109,7 @@ namespace TARpv23_CSharp
                 double pikkus = Double.Parse(Console.ReadLine());
                 string vastus0 = Funktsioonid.Pikkuse_analuus(pikkus);
                 Console.WriteLine("Sinu pikkus on {0} m, sa oled {1}", pikkus, vastus0);
-            } 
+            }
 
             catch (Exception ex)
             {
@@ -116,7 +118,7 @@ namespace TARpv23_CSharp
             string[] nimed = new string[5] { "Anna", "Inna", "Oksana", "Pavel", "Karl" };
             nimed[0] = "Marina";
 
-            
+
             //1.V
             for (int i = 0; i < nimed.Length; i++)
             {
@@ -151,20 +153,20 @@ namespace TARpv23_CSharp
             }
 
             // 1. Loo juhuslikult arvud N ja M ja sisesta massiivi arvud N'st M'ni.Trüki arvude ruudud ekraanile. N ja M arvud on vahemikus(-100,100).
-            int N = random.Next(-10,101);
+            int N = random.Next(-10, 101);
             Console.WriteLine(N);
-            int M = random.Next(-10,101);
+            int M = random.Next(-10, 101);
             Console.WriteLine(M);
             int[] arvud;
             if (N < M)
             {
-                arvud=Funktsioonid.Arvude_massiv(N, M);
+                arvud = Funktsioonid.Arvude_massiv(N, M);
             }
             else
             {
                 arvud = Funktsioonid.Arvude_massiv(M, N);
             }
-            foreach (int item in arvud)         
+            foreach (int item in arvud)
             {
                 Console.WriteLine(item * item);
             }
@@ -187,7 +189,7 @@ namespace TARpv23_CSharp
             Console.WriteLine("Numbrite summa: " + nende_summa);
             Console.WriteLine("Aritmeetiline keskmine: " + aritmeetiline_keskmine);
             Console.WriteLine("Arvude korrutamine: " + korrutis);
-        
+
             // 3. Küsi viielt kasutajalt nimed ja vanused, salvesta nende andmeid massiivi ning väljasta summaarne vanus, aritmeetiline keskmine, vaanema ja noorema inimeste nimed ja vanused.
             string[] kasutajalt_nimed = new string[5];
             int[] vanuseid = new int[5];
@@ -203,7 +205,7 @@ namespace TARpv23_CSharp
             {
                 summaarne_vanus += vanuseid[i];
             }
-            double keskmine_vanus  = (double)summaarne_vanus / vanuseid.Length;
+            double keskmine_vanus = (double)summaarne_vanus / vanuseid.Length;
             Console.WriteLine("Summaarne vanus: " + summaarne_vanus);
             Console.WriteLine("Keskmine vanus : " + keskmine_vanus);
             Console.WriteLine("\nÜle keskealised inimesed: ");
@@ -225,24 +227,55 @@ namespace TARpv23_CSharp
 
             // 4. Ütle kasutajale "Osta elevant ära!". Senikaua korda küsimust, kuni kasutaja lõpuks ise kirjutab "elevant".
             string Kasutaja_sisestamine = " ";
-            while (Kasutaja_sisestamine.ToLower() != "elevant")
+            do
             {
                 Console.WriteLine("Osta elevant ära!");
                 Kasutaja_sisestamine = Console.ReadLine();
             }
+            while (!string.Equals(Kasutaja_sisestamine, "elevant", StringComparison.OrdinalIgnoreCase));
             Console.WriteLine("Müüdud!");
 
-            // 6. Küsi kasutajalt 4 arvu ning väljasta nendest koostatud suurim neliarvuline arv.
-            int[] neli_numbrit = new int[4];
-            for (int i = 0; i < 4; i++)
+            //5. Mis arv mõtles välja arvuti? Kasuta vähemalt 5 katset, et seda teada.
+            int arvutiArv = random.Next(1, 25);
+            int kasutajaArv;
+            int katsed = 0;
+            bool arvatiOigesti = false;
+            Console.WriteLine("Programm on mõelnud välja arvu vahemikus 1 kuni 50, sul on 5 katset.");
+            while (katsed < 5 && !arvatiOigesti)
             {
-                Console.Write("Sisestage neli numbrit {0}: ", i + 1);
-                neli_numbrit[i] = int.Parse(Console.ReadLine());
+                Console.Write("Sisesta oma arvatav arv: ");
+                kasutajaArv = int.Parse(Console.ReadLine());
+                katsed++;
+                if (kasutajaArv == arvutiArv)
+                {
+                    Console.WriteLine("Õige! Sa arvasid arvu ära.");
+                    arvatiOigesti = true;
+                }
+                else if (kasutajaArv < arvutiArv)
+                {
+                    Console.WriteLine("Liiga väike!");
+                }
+                else
+                {
+                    Console.WriteLine("Liiga suur!");
+                }
             }
-            var numbrite_sorteerimine  = neli_numbrit.OrderByDescending(d => d).ToArray();
-            string suurim_number = string.Concat(numbrite_sorteerimine);
-            Console.WriteLine("Swuurim number: " + suurim_number);
+            if (arvatiOigesti = false)
+            {
+                Console.WriteLine($"Kahjuks ei arvanud sa õiget arvu ära. Programm mõtles välja arvu (arvutiArv).");
+
+                // 6. Küsi kasutajalt 4 arvu ning väljasta nendest koostatud suurim neliarvuline arv.
+                int[] neli_numbrit = new int[4];
+                for (int i = 0; i < 4; i++)
+                {
+                    Console.Write("Sisestage neli numbrit {0}: ", i + 1);
+                    neli_numbrit[i] = int.Parse(Console.ReadLine());
+                }
+                var numbrite_sorteerimine = neli_numbrit.OrderByDescending(d => d).ToArray();
+                string suurim_number = string.Concat(numbrite_sorteerimine);
+                Console.WriteLine("Swuurim number: " + suurim_number);
+            }
         }
-    }    
+    }
 }
 
